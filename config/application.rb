b1a5109885b443788abd51dc9906528e8103a4d7
Dylan Schwartz <dschwar1@andrew.cmu.edu>
2018-07-12 02:28:26 +0000

    ugh

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-#for heroku
-config.assets.initialize_on_precompile = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -25,5 +22,8 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    #for heroku
+config.assets.initialize_on_precompile = false
   end
 end
