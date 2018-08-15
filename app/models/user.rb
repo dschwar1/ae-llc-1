c6239f40e97d159b26d7564e670251e7371c5fb2
Dylan Schwartz <dschwar1@andrew.cmu.edu>
2018-08-15 22:40:01 +0000

    fixed regex error for email in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     has_secure_password
     
     #Validations
-    validates :email, presence: true, uniqueness: { case_sensitive: false}, format: { with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format" }
+    validates :email, presence: true, uniqueness: { case_sensitive: false}, format: { with: /\A([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format" }
     validates :phone, format: { with: /\A\(?\d{3}\)?[-. ]?\d{3}[-.]?\d{4}\z/, message: "should be 10 digits (area code needed) and delimited with dashes only", allow_blank: true }
     validates :role, inclusion: { in: %w[admin foreman], message: "is not a recognized role in system" }
     validates_presence_of :password, :first_name, :last_name
